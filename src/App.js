4469af6422759f186417ee5415333d30cb634d70
Mythage <so.nijdam@gmail.com>
2023-01-14 16:07:02 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { ReactComponent as ShoppingCart } from './assets/winkelmandje.svg';
 import citroenen from './assets/citroenen.jpeg';
 import limoenen from './assets/limoenen.png';
 import ijsblokjes from './assets/ijsblokjes.jpg';
-import { ReactComponent as ShoppingCart} from "./assets/winkelmandje.svg";
+import Product from './components/Product';
 import './App.css';
 
 function App() {
@@ -14,11 +15,11 @@ function App() {
         <>
             <nav>
                 <ul>
-                    <li><a href={"/"}>Shop</a></li>
-                    <li><a href={"/"}>Ons verhaal</a></li>
-                    <li><a href={"/"}>Blog</a></li>
+                    <li><a href="/">Shop</a></li>
+                    <li><a href="/">Ons verhaal</a></li>
+                    <li><a href="/">Blog</a></li>
                 </ul>
-                <ShoppingCart className='shopping-cart-icon'/>
+                <ShoppingCart className="shopping-cart-icon"/>
             </nav>
             <header>
                 <h1>Fruit perfection</h1>
@@ -26,43 +27,28 @@ function App() {
                     Shop nu
                 </button>
             </header>
-        <main>
-            <article className={'product'}>
-                <img
-                    src={citroenen}
-                    alt="Ciroen"
-                />
-                <h2 className={'product-name'}>Cirtoen</h2>
-                <p className={'product-description'}>
-                    "Een citroen is voor de meeste mensen te zuur om zo uit de hand te eten. Van citroen kun je het
-                    vruchtvlees, het sap en de schil gebruiken.
+            <main>
+                <Product
+                    image={citroenen}
+                    title="Citroen"
+                    description="Een citroen is voor de meeste mensen te zuur om zo uit de hand te eten.
+                    Van citroen kun je het vruchtvlees, het sap en de schil gebruiken.
                     Het sappige, lichtgele zure vruchtvlees versterkt de smaak van ander voedsel."
-                </p>
-            </article>
-            <article className={'product'}>
-                <img
-                    src={limoenen}
-                    alt="Limoen"
                 />
-                <h2 className={'product-name'}>Limoen</h2>
-                <p className={'product-description'}>
-                    Limoen is familie van de citroen en de sinaasappel en behoort tot de citrusvruchten (Wijnruitfamilie).
-                    Limoenen zijn rond en kleiner dan citroenen. De schil is dun, vrij glad en groen.
-                </p>
-            </article>
-            <article className={'product'}>
-                <img
-                    src={ijsblokjes}
-                    alt="ijsblokjes"
+                <Product
+                    image={limoenen}
+                    title="Limoen"
+                    description="Limoen is familie van de citroen en de sinaasappel en behoort tot de citrusvruchten (Wijnruitfamilie).
+                    Limoenen zijn rond en kleiner dan citroenen.
+                    De schil is dun, vrij glad en groen."
                 />
-                <h2 className={'product-name'}>Ijsblokjes</h2>
-                <p className={'product-description'}>
-                    Een ijsblokje of ijsklontje is bevroren water in de vorm van een klein blokje.
-                    Het wordt gemaakt in een diepvriezer door water in een plastic vorm te laten bevriezen.
-                </p>
-            </article>
-
-        </main>
+                <Product
+                    image={ijsblokjes}
+                    title="Limoen"
+                    description="Een ijsblokje of ijsklontje is bevroren water in de vorm van een klein blokje.
+                    Het wordt gemaakt in een diepvriezer door water in een plastic vorm te laten bevriezen."
+                />
+            </main>
         </>
     );
 }
